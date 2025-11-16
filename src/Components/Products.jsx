bf55632174825331ff3207e863c4bca0b476b4bb
Danish Nawaz <140861405+DAlgoSculptor@users.noreply.github.com>
2025-11-17 00:48:33 +0530

    first commit

---
+++ src/Components/Products.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Products = () =>{
+    return(
+        <>
+        <p className="text-center text-emrald-500 py-3 px-4 bg-rose-900">World Mange Coder Like me !!</p>
+        <div className="w-full h-screen bg-zinc-900 text-white flex px-10 py-10 text-center ">
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        <div>World requare more coder like me !!</div>
+        </div>
+        </>
+        
+    )
+}
+
+export default Products
