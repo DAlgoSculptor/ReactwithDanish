9c0dbb9bae4e4d171495f42a6f4b21ad6626cb51
Danish Nawaz <140861405+DAlgoSculptor@users.noreply.github.com>
2025-11-17 22:21:15 +0530

    All updated

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,23 +1,13 @@
-import React from "react";
-
 const Products = () =>{
     return(
         <>
-        <p className="text-center text-emrald-500 py-3 px-4 bg-rose-900">World Mange Coder Like me !!</p>
-        <div className="w-full h-screen bg-zinc-900 text-white flex px-10 py-10 text-center ">
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
-        <div>World requare more coder like me !!</div>
+        <div className="w-full h-screen bg-zinc-800 p-4 py-10 flex text-white">
+        <div>Hello world!!</div>
+        <div>Hello world!!</div>
+        <div>Hello world!!</div>
+        <div>Hello world!!</div>
         </div>
         </>
-        
     )
 }
-
 export default Products
